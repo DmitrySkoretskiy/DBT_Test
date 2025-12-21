d4a7d82cdf9b4152ff17674b87d36d0670930640
dmytroskoretskyi <dmytro.skoretskyi@atlaslive.tech>
2025-12-21 17:25:22 +0200

    seeds

--- models/intermediate/fligths/fct_tickets.sql
+++ models/intermediate/fligths/fct_tickets.sql
@@ -13,4 +13,10 @@ select
     passenger_name, 
     contact_data
 from
-    {{ ref('stg_fligths__tickets') }}
+    {{ ref('stg_fligths__tickets') }} sft
+    where 
+      sft.passenger_id not in
+      (
+        select passenger_id
+        from {{ ref('staff') }}
+      )
